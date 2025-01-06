1cf85c473312922499f5402b80ee0bc6173c3384
TshepoMagagula <tshepomagagulatp11@gmail.com>
2025-01-06 11:43:09 +0200

    Added the watch movie page (where users can watch the movie)

---
+++ src/Pages/watchMovie.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const movieVideo = () => {
+    return (
+        <>
+            <h1>Watch Movie</h1>
+            <p>Video goes here</p>
+            <p>Resolution: MP4...</p>
+        </>
+    );
+}
+
+export default movieVideo;
